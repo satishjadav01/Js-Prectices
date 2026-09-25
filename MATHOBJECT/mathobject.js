75522549a5f17ca0e6ed87c72b90aafdc29c3a06
Satish Jadav <satishjadav700@gmail.com>
2026-09-25 12:35:15 +0530

    done

---
+++ MATHOBJECT/mathobject.js
@@ -0,0 +1,41 @@
+//! What is Math object ?? 
+//* math object which is used to perform the mathematical operation.
+
+//? property - PI
+
+//? Methods 
+
+//! 1 round ()
+//* do the round figer 
+
+console.log(Math.round(4.67));
+console.log(Math.round(4.1));
+
+//! 2) Math.floor()
+//* remove the poits 
+console.log(Math.floor(8.1))
+
+//! 3) sqrt()
+//* sqrt the printout the sqrt
+
+console.log(Math.sqrt(49));
+
+//! 4) cbrt()
+console.log(Math.cbrt(49));
+//*  printout the cbrt
+
+//! 5) min()
+//* return smallest value 
+console.log(Math.min(10,20,30))
+
+//! 5) max()
+//* return biggest value 
+console.log(Math.max(10,20,30))
+
+//! 6) random ()
+//* return random number 
+console.log(Math.random())
+
+
+// generate otp 
+
